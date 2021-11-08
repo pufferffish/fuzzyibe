114418d6c2f5e23764a037668928e0e42f234e42
octeep <octeep@pm.me>
2021-11-08 04:21:00 +0000

    key generation

--- app/FuzzyIBE/Main.hs
+++ app/FuzzyIBE/Main.hs
@@ -1,20 +1,20 @@
+{-# LANGUAGE TupleSections #-}
+
 module Main where
 
 import Protolude
 
-import Data.Group (pow, invert)
-import Data.Curve.Weierstrass (Point(A), gen)
-import qualified Data.Curve.Weierstrass as W
-
-import Data.Pairing.BLS12381 (BLS12381, G1, G2, pairing, Fr)
 import Data.Curve (Curve)
+import Data.Curve.Weierstrass (Point(A), gen)
 import Data.Field.Galois (fromP, toP, Prime, PrimeField)
-
+import Data.Group (pow, invert)
+import Data.List ((!!), delete)
+import Data.Map (Map)
+import Data.Pairing.BLS12381 (BLS12381, G1, G2, pairing, Fr)
 import Data.Set (Set)
-import qualified Data.Set as Set
-
 import System.Random
-import Data.List ((!!))
+import qualified Data.Curve.Weierstrass as W
+import qualified Data.Map as Map
 
 mul' :: (Curve f c e q r, PrimeField n) => Point f c e q r -> n -> Point f c e q r
 mul' p = W.mul' p . fromP
@@ -31,27 +31,38 @@ g2 = mul' gen (3857874365743587436587435743658743 :: Fr) -- randomly chosen
 p :: G1 BLS12381
 p = mul' gen (3432432432432432432 :: Fr)
 
-
 q :: G2 BLS12381
 q = mul' gen (837587436533242 :: Fr)
 
 -- Langrange coefficient
-delta :: Fr -> Set Fr -> Fr -> Fr
-delta i s x = product $ f <$> Set.toList (Set.delete i s)
+delta :: Fr -> [Fr] -> Fr -> Fr
+delta i s x = product $ f <$> delete i s
     where f j = (x - j) / (i - j)
 
 capitalT :: [G1 BLS12381] -> G1 BLS12381 -> Fr -> G1 BLS12381 
-capitalT t g2 x = a <> b
-    where a = pow g2 $ fromP (x * toP n)
-          b = fold (f <$> n')
-          f (t', i) = pow t' (delta i n'' x)
-          n = toInteger $ length t 
-          n' = zip t [1..]
-          n'' = Set.fromList $ toP <$> [1..(n+1)]
+capitalT t g2 x = pow g2 (fromP (x * toP n)) <> fold (f <$> n')
+    where  
+        f (t', i) = pow t' (delta i n'' x)
+        n = toInteger $ length t 
+        n' = zip t [1..]
+        n'' = toP <$> [1..(n+1)]
+
+data PrivateKey = PrivateKey [G1 BLS12381] [G1 BLS12381]
+
+keyGeneration :: Int -> G1 BLS12381 -> [G1 BLS12381] -> Fr -> [Fr] -> IO PrivateKey 
+keyGeneration d g2 t y w = do
+    cef <- (y :) <$> replicateM (d-1) (randomIO :: IO Fr)
+    r <- Map.fromList <$> mapM (\x -> (x,) <$> (randomIO :: IO Fr)) w
+    let q = poly cef
+    let bigD = (\i -> pow g2 (q i) <> pow (capitalT t g2 i) (r Map.! i)) <$> w
+    let smallD = (\i -> pow g2 (r Map.! i)) <$> w
+    return $ PrivateKey bigD smallD
+    where 
+        poly cef x = sum $ (\(a,b) -> a * pow x b) <$> zip cef [0..]
 
 main :: IO ()
 main = do
-  let n = 4
+  let n = 11
   t <- replicateM n (randomIO :: IO (G1 BLS12381))
   print $ capitalT t g2 10
   putText "P:"
